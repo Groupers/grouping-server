77bda931324644a0575e1c2e1861859f1355d6da
jhlee <ljh50333037@gmail.com>
2020-06-15 19:29:00 +0900

    Add UserController

--- src/main/java/com/covengers/grouping/controller/UserController.java
+++ src/main/java/com/covengers/grouping/controller/UserController.java
@@ -1,15 +1,28 @@
 package com.covengers.grouping.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-
-import com.covengers.grouping.controller.AppApiV1Controller;
-
+import com.covengers.grouping.component.CommonResponseMaker;
+import com.covengers.grouping.dto.BringCrewListDto;
+import com.covengers.grouping.dto.CommonResponse;
+import com.covengers.grouping.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RequiredArgsConstructor
 @RestController
 public class UserController extends AppApiV1Controller {
+    private final UserService userService;
+    private final CommonResponseMaker commonResponseMaker;
 
+    @GetMapping("/users/groups/user-id")
+    public CommonResponse<BringCrewListDto> bringCrewList(@RequestParam("user-id") String userId) {
+
+        final BringCrewListDto responseDto =
+                BringCrewListDto.of(userService.bringCrewList(userId));
+
+        return commonResponseMaker.makeSucceedCommonResponse(responseDto);
+    }
 }
