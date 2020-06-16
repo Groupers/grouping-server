4daca25183d578475bb47203fac66b8a3e546eb3
jhlee <ljh50333037@gmail.com>
2020-06-16 18:44:51 +0900

    Add Group, EnrollGroupRequest DTO and VO

--- src/main/java/com/covengers/grouping/dto/EnrollGroupRequestDto.java
+++ src/main/java/com/covengers/grouping/dto/EnrollGroupRequestDto.java
@@ -1,6 +1,7 @@
 package com.covengers.grouping.dto;
 
 import com.covengers.grouping.constant.Gender;
+import com.covengers.grouping.dto.vo.EnrollGroupRequestVo;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
@@ -18,4 +19,18 @@ public class EnrollGroupRequestDto {
     private Long pointX;
     private Long pointY;
     private String pointDescription;
+
+    public EnrollGroupRequestVo toVo(){
+        return EnrollGroupRequestVo.builder()
+                .title(title)
+                .maxUserNumber(maxUserNumber)
+                .minUserAge(minUserAge)
+                .maxUserAge(maxUserAge)
+                .availableGender(availableGender)
+                .description(description)
+                .pointX(pointX)
+                .pointY(pointY)
+                .pointDescription(pointDescription)
+                .build();
+    }
 }
