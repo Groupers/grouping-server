1639b6fd65dd21e7f59868cd1f398470d7d70ec5
jhlee <ljh50333037@gmail.com>
2020-06-16 11:24:22 +0900

    modify method name to getCrewList

--- src/main/java/com/covengers/grouping/controller/UserController.java
+++ src/main/java/com/covengers/grouping/controller/UserController.java
@@ -1,13 +1,13 @@
 package com.covengers.grouping.controller;
 
 import com.covengers.grouping.component.CommonResponseMaker;
-import com.covengers.grouping.dto.BringCrewListDto;
+import com.covengers.grouping.dto.CrewListResponseDto;
 import com.covengers.grouping.dto.CommonResponse;
 import com.covengers.grouping.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -17,11 +17,11 @@ public class UserController extends AppApiV1Controller {
     private final UserService userService;
     private final CommonResponseMaker commonResponseMaker;
 
-    @GetMapping("/users/groups/user-id")
-    public CommonResponse<BringCrewListDto> bringCrewList(@RequestParam("user-id") String userId) {
+    @GetMapping("/users/groups/{userId}")
+    public CommonResponse<CrewListResponseDto> bringCrewList(@PathVariable("userId") String userId) {
 
-        final BringCrewListDto responseDto =
-                BringCrewListDto.of(userService.bringCrewList(userId));
+        final CrewListResponseDto responseDto =
+                CrewListResponseDto.of(userService.getCrewList(userId));
 
         return commonResponseMaker.makeSucceedCommonResponse(responseDto);
     }
