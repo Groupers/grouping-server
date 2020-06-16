b7541e35f19e76c5b71ad6d48aae367364919ed6
jhlee <ljh50333037@gmail.com>
2020-06-16 17:20:24 +0900

    Rename Crew to Group

---
+++ src/main/java/com/covengers/grouping/dto/EnrollGroupRequestDto.java
@@ -0,0 +1,21 @@
+package com.covengers.grouping.dto;
+
+import com.covengers.grouping.constant.Gender;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@Builder
+@ToString
+public class EnrollGroupRequestDto {
+    private String title;
+    private Integer maxUserNumber;
+    private Integer minUserAge;
+    private Integer maxUserAge;
+    private Gender availableGender;
+    private String description;
+    private Long pointX;
+    private Long pointY;
+    private String pointDescription;
+}
