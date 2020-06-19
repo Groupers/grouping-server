353707ca13d84432a1fa4f1474d2451874f09946
jhlee <ljh50333037@gmail.com>
2020-06-19 18:44:47 +0900

    Add @Transactional annotation

--- src/main/java/com/covengers/grouping/service/GroupService.java
+++ src/main/java/com/covengers/grouping/service/GroupService.java
@@ -9,6 +9,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -16,6 +18,7 @@ public class GroupService {
     private final RedisTemplate<String, String> redisTemplate;
     private final GroupRepository groupRepository;
 
+    @Transactional
     public GroupVo createGroup(CreateGroupRequestVo requestVo) {
 
         Group group = new Group(requestVo.getTitle(),
