970ff1f4b7eaac6a677be99d7527f46a24e59186
jhlee <ljh50333037@gmail.com>
2020-06-23 12:00:45 +0900

    modify recommendGroup method

--- src/main/java/com/covengers/grouping/service/GroupService.java
+++ src/main/java/com/covengers/grouping/service/GroupService.java
@@ -1,25 +1,31 @@
 package com.covengers.grouping.service;
 
-import com.covengers.grouping.component.Word2VecRecommendMaker;
+import com.covengers.grouping.component.HashtagRecommender;
 import com.covengers.grouping.domain.Group;
 import com.covengers.grouping.dto.vo.CreateGroupRequestVo;
 import com.covengers.grouping.dto.vo.GroupVo;
 import com.covengers.grouping.dto.vo.RecommendGroupVo;
+import com.covengers.grouping.dto.vo.RecommendHashtagVo;
 import com.covengers.grouping.repository.GroupRepository;
+import com.covengers.grouping.repository.HashtagRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class GroupService {
     private final RedisTemplate<String, String> redisTemplate;
-    private final Word2VecRecommendMaker word2VecRecommendMaker;
+    private final HashtagRecommender hashtagRecommender;
     private final GroupRepository groupRepository;
+    private final HashtagRepository hashtagRepository;
 
     @Transactional
     public GroupVo createGroup(CreateGroupRequestVo requestVo) {
@@ -41,8 +47,22 @@ public class GroupService {
 
     public RecommendGroupVo recommendGroup(String keyword){
 
-        RecommendGroupVo recommendGroupVo = word2VecRecommendMaker.recommend(keyword);
+        final RecommendHashtagVo recommendHashtagVo = hashtagRecommender.recommend(keyword);
 
-        return recommendGroupVo;
+        HashSet<GroupVo> groupHashSet = new HashSet<>();
+
+        for (String hashtag : recommendHashtagVo.getHashtagList()) {
+            List<GroupVo> list =
+            hashtagRepository.findByHashtag(hashtag).get().toGroupList();
+            for (GroupVo groupVo : list) {
+                groupHashSet.add(groupVo);
+            }
+        }
+
+        return RecommendGroupVo
+                .builder()
+                .groupList(groupHashSet.stream()
+                            .collect(Collectors.toList()))
+                .build();
     }
 }
