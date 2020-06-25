44dd77ccabd3fd5d9631de304c5b68b8b1002317
jhlee <ljh50333037@gmail.com>
2020-06-25 16:43:44 +0900

    Add UserFriendMapping domain

---
+++ grouping-core/src/main/java/com/covengers/grouping/domain/UserFriendMapping.java
@@ -0,0 +1,30 @@
+package com.covengers.grouping.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@EqualsAndHashCode(of = "id", callSuper = false)
+@Table(name = "user_friend_mapping")
+public class UserFriendMapping extends AbstractAuditingEntity {
+
+    private static final long serialVersionUID = -2710275756256638368L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_friend_mapping_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "grouping_user_id")
+    private GroupingUser groupingUser;
+    @ManyToOne
+    @JoinColumn(name = "friend_id")
+    private GroupingUser friendUser;
+}
