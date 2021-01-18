74e7c5f7d6f553cdd57a38b50239a5af901b1a98
kseungwoo <seungwookim.dev@gmail.com>
2021-01-18 17:06:07 +0900

    create prototype method of findKeywordByCreatedAtAfter

--- grouping-core/src/main/java/com/covengers/grouping/repository/KeywordRepository.java
+++ grouping-core/src/main/java/com/covengers/grouping/repository/KeywordRepository.java
@@ -3,5 +3,10 @@ package com.covengers.grouping.repository;
 import com.covengers.grouping.domain.Keyword;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface KeywordRepository extends JpaRepository<Keyword, Long> {
+
+    List<String> findKeywordByCreatedAtAfter(LocalDateTime createdAt);
 }
