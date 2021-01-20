905417f23b4c13c979fb5e1ec303ccf468981f44
김승우 <kseungwoo@gimseung-uui-MacBookAir.local>
2021-01-20 16:22:53 +0900

    embedded_redis for m1 mac

--- grouping-api/src/main/java/com/covengers/grouping/configuration/EmbeddedRedisConfiguration.java
+++ grouping-api/src/main/java/com/covengers/grouping/configuration/EmbeddedRedisConfiguration.java
@@ -2,11 +2,13 @@ package com.covengers.grouping.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ClassPathResource;
 import redis.embedded.RedisServer;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import java.io.File;
+import java.io.IOException;
 
 @Configuration
 public class EmbeddedRedisConfiguration {
@@ -17,8 +19,20 @@ public class EmbeddedRedisConfiguration {
     private RedisServer redisServer;
 
     @PostConstruct
-    public void redisServer() {
-        redisServer = new RedisServer(redisPort);
+    public void redisServer() throws IOException {
+
+        String arch = System.getProperty("os.arch");
+        System.out.println("arch = " + arch);
+        String name = System.getProperty("os.name");
+
+        System.out.println("name = " + name);
+
+        if (isArmMac()) {
+            redisServer = new RedisServer(getRedisFileForArmMac(), redisPort);
+        } else {
+            redisServer = new RedisServer(redisPort);
+        }
+
         redisServer.start();
     }
 
@@ -27,5 +41,15 @@ public class EmbeddedRedisConfiguration {
         if (redisServer != null) {
             redisServer.stop();
         }
+    }
+
+    private boolean isArmMac() {
+        return (System.getProperty("os.arch") == "aarch64") &&
+                (System.getProperty("os.name") == "Mac OS X");
+    }
+
+    private File getRedisFileForArmMac() throws IOException {
+        ClassPathResource resource = new ClassPathResource("binary/redis/redis-server-6.0.10-mac-arm64");
+        return resource.getFile();
     }
 }
