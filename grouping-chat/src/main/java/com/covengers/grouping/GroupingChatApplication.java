e4a4b762710c1141a3eec0539f0e874b354b1f7e
김승우 <seungwookim.dev@gmail.com>
2021-02-03 15:41:59 +0900

    update

---
+++ grouping-chat/src/main/java/com/covengers/grouping/GroupingChatApplication.java
@@ -0,0 +1,13 @@
+package com.covengers.grouping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupingChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupingChatApplication.class, args);
+    }
+
+}
