ab549dea8c72e4f692b9a2b53e30e683e986e327
jeonsang <user@AL01713664.local>
2021-02-03 22:47:51 +0900

    [NO-BTS] Copy Sign Controller logic to GateWay

--- grouping-gw/src/main/java/com/covengers/grouping/adapter/api/GroupingApiClient.java
+++ grouping-gw/src/main/java/com/covengers/grouping/adapter/api/GroupingApiClient.java
@@ -1,22 +1,37 @@
 package com.covengers.grouping.adapter.api;
 
-import com.covengers.grouping.adapter.api.dto.*;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.covengers.grouping.adapter.api.dto.CreateGroupCompleteRequestDto;
+import com.covengers.grouping.adapter.api.dto.FriendListResponseDto;
+import com.covengers.grouping.adapter.api.dto.GroupListResponseDto;
 import com.covengers.grouping.adapter.api.dto.GroupResponseDto;
+import com.covengers.grouping.adapter.api.dto.GroupingUserResponseDto;
+import com.covengers.grouping.adapter.api.dto.RecommendGroupResponseDto;
+import com.covengers.grouping.adapter.api.dto.ResetPasswordCompleteRequestDto;
+import com.covengers.grouping.adapter.api.dto.SearchHistoryListResponseDto;
+import com.covengers.grouping.adapter.api.dto.SearchTrendsListResponseDto;
 import com.covengers.grouping.adapter.api.dto.SignInWithEmailRequestDto;
 import com.covengers.grouping.adapter.api.dto.SignInWithPhoneNumberRequestDto;
 import com.covengers.grouping.adapter.api.dto.SignUpCheckEmailResponseDto;
 import com.covengers.grouping.adapter.api.dto.SignUpCheckPhoneNumberResponseDto;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
-
+import com.covengers.grouping.adapter.api.dto.SignUpCompleteRequestDto;
 import com.covengers.grouping.configuration.FeignConfiguration;
-import org.springframework.web.multipart.MultipartFile;
 
 @FeignClient(value = "grouping-api",
         url = "${grouping.url.api}",
         configuration = {
                 FeignConfiguration.class,
+                GroupingApiRequestInterceptor.class,
         })
 @RequestMapping(
         consumes = MediaType.APPLICATION_JSON_VALUE,
@@ -25,7 +40,8 @@ import org.springframework.web.multipart.MultipartFile;
 public interface GroupingApiClient {
 
     @GetMapping(path = "/sign/email")
-    GroupingApiAdapterResponse<SignUpCheckEmailResponseDto> checkSignUpEmail(@RequestParam("email") String email);
+    GroupingApiAdapterResponse<SignUpCheckEmailResponseDto> checkSignUpEmail(
+            @RequestParam("email") String email);
 
     @GetMapping(path = "/sign/phone-number")
     GroupingApiAdapterResponse<SignUpCheckPhoneNumberResponseDto> checkSignUpPhoneNumber(
@@ -49,14 +65,16 @@ public interface GroupingApiClient {
     @PostMapping(path = "/group/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     GroupingApiAdapterResponse<GroupResponseDto> uploadGroupImage(
             @RequestPart MultipartFile imageFile,
-            @RequestParam final Long groupId
+            @RequestParam Long groupId
     );
 
     @GetMapping(path = "/group/keyword")
-    GroupingApiAdapterResponse<RecommendGroupResponseDto> recommendGroup(@RequestParam Long groupingUserId, @RequestParam String keyword);
+    GroupingApiAdapterResponse<RecommendGroupResponseDto> recommendGroup(@RequestParam Long groupingUserId,
+                                                                         @RequestParam String keyword);
 
     @GetMapping("/keywords/search/history")
-    GroupingApiAdapterResponse<SearchHistoryListResponseDto> getSearchHistoryList(@RequestParam Long groupingUserId);
+    GroupingApiAdapterResponse<SearchHistoryListResponseDto> getSearchHistoryList(
+            @RequestParam Long groupingUserId);
 
     @GetMapping("/keywords/search/trends")
     GroupingApiAdapterResponse<SearchTrendsListResponseDto> getSearchTrendsList();
@@ -74,5 +92,4 @@ public interface GroupingApiClient {
     @PutMapping("/users/password")
     GroupingApiAdapterResponse<Void> resetPassword(
             @RequestParam Long groupingUserId, @RequestBody ResetPasswordCompleteRequestDto requestDto);
-
 }
--- grouping-gw/src/main/java/com/covengers/grouping/service/AuthService.java
+++ grouping-gw/src/main/java/com/covengers/grouping/service/AuthService.java
@@ -1,29 +1,50 @@
 package com.covengers.grouping.service;
 
-import com.covengers.grouping.adapter.api.dto.*;
-import com.covengers.grouping.adapter.api.dto.GroupResponseDto;
-import com.covengers.grouping.adapter.api.dto.SignInWithEmailRequestDto;
-import com.covengers.grouping.adapter.api.dto.SignInWithPhoneNumberRequestDto;
-import com.covengers.grouping.adapter.api.dto.SignUpCheckEmailResponseDto;
-import com.covengers.grouping.adapter.api.dto.SignUpCheckPhoneNumberResponseDto;
-import com.covengers.grouping.adapter.chat.GroupingChatClient;
-import com.covengers.grouping.adapter.chat.dto.ChatRoomResponseDto;
-import com.covengers.grouping.vo.*;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.covengers.grouping.adapter.api.GroupingApiClient;
+import com.covengers.grouping.adapter.api.dto.CreateGroupCompleteRequestDto;
+import com.covengers.grouping.adapter.api.dto.FriendListResponseDto;
+import com.covengers.grouping.adapter.api.dto.GroupListResponseDto;
+import com.covengers.grouping.adapter.api.dto.GroupResponseDto;
+import com.covengers.grouping.adapter.api.dto.GroupingUserResponseDto;
+import com.covengers.grouping.adapter.api.dto.RecommendGroupResponseDto;
+import com.covengers.grouping.adapter.api.dto.ResetPasswordCompleteRequestDto;
+import com.covengers.grouping.adapter.api.dto.SearchHistoryListResponseDto;
+import com.covengers.grouping.adapter.api.dto.SearchTrendsListResponseDto;
+import com.covengers.grouping.adapter.api.dto.SignInWithEmailRequestDto;
+import com.covengers.grouping.adapter.api.dto.SignInWithPhoneNumberRequestDto;
+import com.covengers.grouping.adapter.api.dto.SignUpCheckEmailResponseDto;
+import com.covengers.grouping.adapter.api.dto.SignUpCheckPhoneNumberResponseDto;
+import com.covengers.grouping.adapter.api.dto.SignUpCompleteRequestDto;
+import com.covengers.grouping.adapter.chat.GroupingChatClient;
+import com.covengers.grouping.adapter.chat.dto.ChatRoomResponseDto;
 import com.covengers.grouping.component.JwtTokenProvider;
+import com.covengers.grouping.vo.ChatRoomResponseVo;
+import com.covengers.grouping.vo.CreateGroupRequestVo;
+import com.covengers.grouping.vo.FriendListResponseVo;
+import com.covengers.grouping.vo.GroupListResponseVo;
+import com.covengers.grouping.vo.GroupResponseVo;
+import com.covengers.grouping.vo.GroupingUserResponseVo;
+import com.covengers.grouping.vo.JwtTokenVo;
+import com.covengers.grouping.vo.RecommendGroupResponseVo;
+import com.covengers.grouping.vo.ResetPasswordRequestVo;
+import com.covengers.grouping.vo.SearchHistoryListResponseVo;
+import com.covengers.grouping.vo.SearchTrendsListResponseVo;
+import com.covengers.grouping.vo.SignInWithEmailRequestVo;
+import com.covengers.grouping.vo.SignInWithPhoneNumberRequestVo;
+import com.covengers.grouping.vo.SignUpCheckEmailResponseVo;
+import com.covengers.grouping.vo.SignUpCheckPhoneNumberResponseVo;
+import com.covengers.grouping.vo.SignUpRequestVo;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
@@ -40,30 +61,14 @@ public class AuthService {
 
     private final PasswordEncoder passwordEncoder;
 
-    private JwtTokenVo generateToken(String phoneOrEmail, String password) {
-
-        final UsernamePasswordAuthenticationToken token =
-                new UsernamePasswordAuthenticationToken(phoneOrEmail, password);
-
-        final Authentication authentication = authenticationManager.authenticate(token);
-
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        final String jwtToken = tokenProvider.generateToken(authentication);
-
-        return JwtTokenVo.builder()
-                .accessToken(jwtToken)
-                .build();
-    }
-
     public SignUpCheckEmailResponseVo checkSignUpEmail(String email) {
 
         final SignUpCheckEmailResponseDto signUpCheckEmailResponseDto =
                 groupingApiClient.checkSignUpEmail(email).getData();
 
         return SignUpCheckEmailResponseVo.builder()
-                .isEmailAvailable(signUpCheckEmailResponseDto.isEmailAvailable())
-                .build();
+                                         .isEmailAvailable(signUpCheckEmailResponseDto.isEmailAvailable())
+                                         .build();
 
     }
 
@@ -73,17 +78,17 @@ public class AuthService {
                 groupingApiClient.checkSignUpPhoneNumber(phoneNumber).getData();
 
         return SignUpCheckPhoneNumberResponseVo.builder()
-                .isPhoneNumberAvailable(
-                        signUpCheckPhoneNumberResponseDto
-                                .isPhoneNumberAvailable())
-                .build();
+                                               .isPhoneNumberAvailable(
+                                                       signUpCheckPhoneNumberResponseDto
+                                                               .isPhoneNumberAvailable())
+                                               .build();
     }
 
     public JwtTokenVo completeSignUp(SignUpRequestVo signUpRequestVo) {
 
         final SignUpCompleteRequestDto signUpCompleteRequestDto =
                 SignUpCompleteRequestDto.of(signUpRequestVo,
-                        passwordEncoder.encode(signUpRequestVo.getPassword()));
+                                            passwordEncoder.encode(signUpRequestVo.getPassword()));
 
         groupingApiClient.completeSignUp(signUpCompleteRequestDto);
 
@@ -109,63 +114,86 @@ public class AuthService {
         groupingApiClient.signInWithPhoneNumber(signInWithPhoneNumberRequestDto);
 
         return generateToken(signInWithPhoneNumberRequestVo.getPhoneNumber(),
-                signInWithPhoneNumberRequestVo.getPassword());
+                             signInWithPhoneNumberRequestVo.getPassword());
+    }
+
+    private JwtTokenVo generateToken(String phoneOrEmail, String password) {
+
+        final UsernamePasswordAuthenticationToken token =
+                new UsernamePasswordAuthenticationToken(phoneOrEmail, password);
+
+        final Authentication authentication = authenticationManager.authenticate(token);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        final String jwtToken = tokenProvider.generateToken(authentication);
+
+        return JwtTokenVo.builder()
+                         .accessToken(jwtToken)
+                         .build();
     }
 
     public GroupResponseVo createGroup(CreateGroupRequestVo requestVo) {
 
-        final com.covengers.grouping.adapter.api.dto.GroupResponseDto groupResponseDto = groupingApiClient.createGroup(CreateGroupCompleteRequestDto.of(requestVo))
-                .getData();
+        final GroupResponseDto groupResponseDto = groupingApiClient.createGroup(
+                CreateGroupCompleteRequestDto.of(requestVo))
+                                                                   .getData();
 
         return groupResponseDto.toVo();
     }
 
     public GroupResponseVo uploadGroupImage(MultipartFile imageFile,
                                             final Long groupId) {
-        final GroupResponseDto groupResponseDto = groupingApiClient.uploadGroupImage(imageFile, groupId).getData();
+        final GroupResponseDto groupResponseDto = groupingApiClient.uploadGroupImage(imageFile, groupId)
+                                                                   .getData();
 
         return groupResponseDto.toVo();
     }
 
     public RecommendGroupResponseVo recommendGroup(Long groupingUserId, String keyword) {
 
-        final RecommendGroupResponseDto recommendGroupResponseDto = groupingApiClient.recommendGroup(groupingUserId, keyword).getData();
+        final RecommendGroupResponseDto recommendGroupResponseDto = groupingApiClient.recommendGroup(
+                groupingUserId, keyword).getData();
 
         return recommendGroupResponseDto.toVo();
     }
 
     public SearchHistoryListResponseVo getSearchHistoryList(Long groupingUserId) {
 
-        final SearchHistoryListResponseDto searchHistoryListResponseDto = groupingApiClient.getSearchHistoryList(groupingUserId).getData();
+        final SearchHistoryListResponseDto searchHistoryListResponseDto =
+                groupingApiClient.getSearchHistoryList(groupingUserId).getData();
 
         return searchHistoryListResponseDto.toVo();
     }
 
     public SearchTrendsListResponseVo getSearchTrendsList() {
 
-        final SearchTrendsListResponseDto searchTrendsListResponseDto = groupingApiClient.getSearchTrendsList().getData();
+        final SearchTrendsListResponseDto searchTrendsListResponseDto =
+                groupingApiClient.getSearchTrendsList().getData();
 
         return searchTrendsListResponseDto.toVo();
     }
 
     public GroupListResponseVo getGroupList(Long groupingUserId) {
 
-        final GroupListResponseDto groupListResponseDto = groupingApiClient.getGroupList(groupingUserId).getData();
+        final GroupListResponseDto groupListResponseDto = groupingApiClient.getGroupList(groupingUserId)
+                                                                           .getData();
 
         return groupListResponseDto.toVo();
     }
 
     public FriendListResponseVo getFriendList(Long groupingUserId) {
 
-        final FriendListResponseDto friendListResponseDto = groupingApiClient.getFriendList(groupingUserId).getData();
-
+        final FriendListResponseDto friendListResponseDto = groupingApiClient.getFriendList(groupingUserId)
+                                                                             .getData();
         return friendListResponseDto.toVo();
     }
 
     public GroupingUserResponseVo checkUserWithEmailAndPhoneNumber(
             String email, String phoneNumber) {
 
-        final GroupingUserResponseDto groupingUserResponseDto = groupingApiClient.checkUserWithEmailAndPhoneNumber(email, phoneNumber).getData();
+        final GroupingUserResponseDto groupingUserResponseDto =
+                groupingApiClient.checkUserWithEmailAndPhoneNumber(email, phoneNumber).getData();
 
         return groupingUserResponseDto.toVo();
     }
@@ -174,7 +202,8 @@ public class AuthService {
             Long groupingUserId, ResetPasswordRequestVo resetPasswordRequestVo) {
 
         final ResetPasswordCompleteRequestDto resetPasswordCompleteRequestDto =
-                ResetPasswordCompleteRequestDto.of(passwordEncoder.encode(resetPasswordRequestVo.getPassword()));
+                ResetPasswordCompleteRequestDto.of(
+                        passwordEncoder.encode(resetPasswordRequestVo.getPassword()));
 
         groupingApiClient.resetPassword(groupingUserId, resetPasswordCompleteRequestDto);
 
@@ -186,7 +215,7 @@ public class AuthService {
     }
 
     public ChatRoomResponseVo enterChatRoom(Long chatRoomId) {
-        ChatRoomResponseDto chatRoomResponseDto = groupingChatClient.enterChatRoom(chatRoomId).getData();
+        final ChatRoomResponseDto chatRoomResponseDto = groupingChatClient.enterChatRoom(chatRoomId).getData();
         return chatRoomResponseDto.toVo();
     }
 
