a44208e75a6434cdb75176173dc75086a113c3be
김승우 <seungwookim.dev@gmail.com>
2021-02-03 15:03:47 +0900

    create grouping-chat module and migrate functions related to chat

---
+++ grouping-chat/src/main/java/com/covengers/grouping/controller/AppChatV1Controller.java
@@ -0,0 +1,9 @@
+package com.covengers.grouping.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@RequestMapping("/v1")
+public class AppChatV1Controller {
+}
--- grouping-chat/src/main/java/com/covengers/grouping/controller/ChatController.java
+++ grouping-chat/src/main/java/com/covengers/grouping/controller/ChatController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-public class ChatController extends AppApiV1Controller {
+public class ChatController extends AppChatV1Controller {
 
     private final ChatService chatService;
     private final CommonResponseMaker commonResponseMaker;
